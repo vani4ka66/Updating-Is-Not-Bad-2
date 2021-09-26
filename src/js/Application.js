3c689329f76cc2393c445953dd70fe481c75172e
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-26 20:32:16 +0300

    Commit 2

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,5 +1,6 @@
 import EventEmitter from "eventemitter3";
 import anime from "animejs";
+
 export default class Application extends EventEmitter {
   static get events() {
     return {
@@ -12,7 +13,7 @@ export default class Application extends EventEmitter {
     this.init();
     this.emit(Application.events.READY);
 
-    this.anime()
+    anime()
   }
 
   init() {
